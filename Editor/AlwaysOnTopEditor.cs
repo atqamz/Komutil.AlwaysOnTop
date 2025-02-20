e2b93dfb87f752912fbf0ce3e4a8398262b48ad9
Atqa Munzir <atqamz@gmail.com>
2025-02-20 18:06:41 +0700

    chore: clean up

--- Editor/AlwaysOnTopEditor.cs
+++ Editor/AlwaysOnTopEditor.cs
@@ -20,13 +20,13 @@ namespace Komutil.AlwaysOnTop.Editor
         private void DelayComponentReorder()
         {
             EditorApplication.delayCall -= DelayComponentReorder;
-            
+
             if (target is IAlwaysOnTop alwaysOnTopComponent)
             {
                 var components = (alwaysOnTopComponent as MonoBehaviour).GetComponents<Component>();
 
                 if (components.Length > 1 && components[1].GetType() != alwaysOnTopComponent.GetType())
-                    for (int i = 0; i < components.Length - 1; i++)
+                    for (var i = 0; i < components.Length - 1; i++)
                         UnityEditorInternal.ComponentUtility.MoveComponentUp(alwaysOnTopComponent as Component);
             }
         }
